b515084e59b04723b66f5746f0f39690da184dea
Mario Raposo <mario.raposo@gmail.com>
2026-04-03 16:35:00 +0200

    helloworld functie

--- functions/helloworld.ts
+++ functions/helloworld.ts
@@ -1,3 +1,7 @@
-export function onRequest(context) {
-  return new Response("Hello, world!");
-}
+//import type { PagesFunction } from '@cloudflare/workers-types';
+
+export const onRequest: PagesFunction = async (context: any) => {
+  const url = new URL(context.request.url);
+
+ return new Response("Hello, world!");
+};
